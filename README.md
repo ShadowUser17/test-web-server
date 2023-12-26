9cbfd26885db8d7c7a88fd13a6b49887b193608a
Dmytro <39201581+ShadowUser17@users.noreply.github.com>
2023-12-26 17:57:00 +0200

    Migrate to github.com/gin-gonic/gin (#3)

---
+++ README.md
@@ -0,0 +1,4 @@
+#### URLs:
+- [gin](https://github.com/gin-gonic/gin/tree/v1.9.1)
+- [cobra](https://github.com/spf13/cobra/tree/v1.8.0)
+- [prometheus](https://github.com/prometheus/client_golang/tree/v1.17.0)
--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,13 @@
 package main
 
 import (
-	"os"
+	_ "github.com/gin-gonic/gin"
+	_ "github.com/spf13/cobra"
 
-	"github.com/ShadowUser17/TestWebServer/pkg/args"
-	"github.com/ShadowUser17/TestWebServer/pkg/cert"
-	"github.com/ShadowUser17/TestWebServer/pkg/server"
+	_ "github.com/prometheus/client_golang/prometheus/collectors"
+	_ "github.com/prometheus/client_golang/prometheus"
+	_ "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	var params = new(args.Args).Parse()
-	var router = server.GetRouter(*params.Location)
-	var srv = server.GetServer(*params.Address, router)
-	var err error
-
-	if *params.SSLmode {
-		if !params.CertIsExist() {
-			if _, _, err := cert.MakeCert("./", "localhost"); err != nil {
-				router.Logger.Printf("Error: %v\n", err)
-				os.Exit(2)
-			}
-		}
-
-		router.Logger.Printf("Listen: https://%s%s\n", *params.Address, *params.Location)
-		err = srv.ListenAndServeTLS(*params.SSLcert, *params.SSLkey)
-
-	} else {
-		router.Logger.Printf("Listen: http://%s%s\n", *params.Address, *params.Location)
-		err = srv.ListenAndServe()
-	}
-
-	if err != nil {
-		router.Logger.Printf("Error: %v\n", err)
-		os.Exit(1)
-	}
 }
